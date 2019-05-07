d19407601ed2ebafd74ed88e1eaadb1a525636b2
Anatoly <none>
2019-05-07 07:37:58 +0300

    Added db init script

---
+++ Database/Error.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Error]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [DateCreated] DATETIME NOT NULL, 
+    [ShortDesc] NVARCHAR(255) NOT NULL, 
+    [Description] NVARCHAR(255) NOT NULL, 
+    [UserId] INT NOT NULL, 
+    [StatusId] INT NOT NULL, 
+    [PriorityId] INT NOT NULL, 
+    [ImpactId] INT NOT NULL, 
+)
